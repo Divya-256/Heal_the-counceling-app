e416f7775e0811ad528d0fed39f6821f957c8367
Divya-256 <76658875+Divya-256@users.noreply.github.com>
2021-09-11 13:08:36 +0530

    New commit

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax13widget/GeneratedIPhone11ProMax13Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatedline31widget/GeneratedLine31Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax7widget/GeneratedIPhone11ProMax7Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax15widget/GeneratedIPhone11ProMax15Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax14widget/GeneratedIPhone11ProMax14Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax16widget/GeneratedIPhone11ProMax16Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax5widget/GeneratedIPhone11ProMax5Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax11widget/GeneratedIPhone11ProMax11Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax9widget/GeneratedIPhone11ProMax9Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax3widget/GeneratedIPhone11ProMax3Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax4widget/GeneratedIPhone11ProMax4Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax17widget/GeneratedIPhone11ProMax17Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax2widget/GeneratedIPhone11ProMax2Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax1widget/GeneratedIPhone11ProMax1Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatedellipse7widget/GeneratedEllipse7Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax6widget/GeneratedIPhone11ProMax6Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax10widget/GeneratedIPhone11ProMax10Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax8widget/GeneratedIPhone11ProMax8Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax12widget/GeneratedIPhone11ProMax12Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax17widget1/GeneratedIPhone11ProMax17Widget1.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax18widget/GeneratedIPhone11ProMax18Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax19widget/GeneratedIPhone11ProMax19Widget.dart';
+import 'package:flutterapp/heal_the_20counceling_20appapp/generatediphone11promax20widget/GeneratedIPhone11ProMax20Widget.dart';
+
+void main() {
+  runApp(Heal_the_20counceling_20appApp());
+}
+
+class Heal_the_20counceling_20appApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedIPhone11ProMax1Widget',
+      routes: {
+        '/GeneratedIPhone11ProMax13Widget': (context) =>
+            GeneratedIPhone11ProMax13Widget(),
+        '/GeneratedLine31Widget': (context) => GeneratedLine31Widget(),
+        '/GeneratedIPhone11ProMax7Widget': (context) =>
+            GeneratedIPhone11ProMax7Widget(),
+        '/GeneratedIPhone11ProMax15Widget': (context) =>
+            GeneratedIPhone11ProMax15Widget(),
+        '/GeneratedIPhone11ProMax14Widget': (context) =>
+            GeneratedIPhone11ProMax14Widget(),
+        '/GeneratedIPhone11ProMax16Widget': (context) =>
+            GeneratedIPhone11ProMax16Widget(),
+        '/GeneratedIPhone11ProMax5Widget': (context) =>
+            GeneratedIPhone11ProMax5Widget(),
+        '/GeneratedIPhone11ProMax11Widget': (context) =>
+            GeneratedIPhone11ProMax11Widget(),
+        '/GeneratedIPhone11ProMax9Widget': (context) =>
+            GeneratedIPhone11ProMax9Widget(),
+        '/GeneratedIPhone11ProMax3Widget': (context) =>
+            GeneratedIPhone11ProMax3Widget(),
+        '/GeneratedIPhone11ProMax4Widget': (context) =>
+            GeneratedIPhone11ProMax4Widget(),
+        '/GeneratedIPhone11ProMax17Widget': (context) =>
+            GeneratedIPhone11ProMax17Widget(),
+        '/GeneratedIPhone11ProMax2Widget': (context) =>
+            GeneratedIPhone11ProMax2Widget(),
+        '/GeneratedIPhone11ProMax1Widget': (context) =>
+            GeneratedIPhone11ProMax1Widget(),
+        '/GeneratedEllipse7Widget': (context) => GeneratedEllipse7Widget(),
+        '/GeneratedIPhone11ProMax6Widget': (context) =>
+            GeneratedIPhone11ProMax6Widget(),
+        '/GeneratedIPhone11ProMax10Widget': (context) =>
+            GeneratedIPhone11ProMax10Widget(),
+        '/GeneratedIPhone11ProMax8Widget': (context) =>
+            GeneratedIPhone11ProMax8Widget(),
+        '/GeneratedIPhone11ProMax12Widget': (context) =>
+            GeneratedIPhone11ProMax12Widget(),
+        '/GeneratedIPhone11ProMax17Widget1': (context) =>
+            GeneratedIPhone11ProMax17Widget1(),
+        '/GeneratedIPhone11ProMax18Widget': (context) =>
+            GeneratedIPhone11ProMax18Widget(),
+        '/GeneratedIPhone11ProMax19Widget': (context) =>
+            GeneratedIPhone11ProMax19Widget(),
+        '/GeneratedIPhone11ProMax20Widget': (context) =>
+            GeneratedIPhone11ProMax20Widget(),
+      },
+    );
+  }
+}
